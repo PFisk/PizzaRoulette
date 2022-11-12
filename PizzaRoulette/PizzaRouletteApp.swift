5fcfa616fb851e6f98254f090d4ed629426f16ec
PFisk <philip.fisker@visma.com>
2022-11-12 15:54:09 +0100

    Initial Commit

---
+++ PizzaRoulette/PizzaRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaRouletteApp.swift
+//  PizzaRoulette
+//
+//  Created by Philip Fisker on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PizzaRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
